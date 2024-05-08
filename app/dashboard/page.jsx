c9556f4ed0dc7f4230c407a4bc4b06a75ad08d2a
Iischeese <129678221+Iischeese@users.noreply.github.com>
2024-05-08 15:06:11 +0000

    name

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -17,12 +17,23 @@ export default async function PrivatePage() {
 
   const { data, error } = await supabase
     .from('users')
-    .select('first_name')
+    .select()
     
   return (
     <>
       <section className='h-screen w-screen flex flex-col items-start justify-center px-10'>
-        <h1 className='text-5xl'>Hello, {data.map((user,i) => {user.first_name})}</h1>
+        <span className='text-5xl'>Hello, 
+        {data.map((user, i)=>{
+          return(
+            < h1 key={i}>
+            {user.first_name}
+            {" "}
+            {user.last_name}
+            </h1>
+          )
+        })}
+        </span>
+        <h2 className='text-3xl'>Welcome to your classroom</h2>
       </section>
     </>
   )
