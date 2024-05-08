8a9c373a95d2f24a04676fc5a5d7cee1048472f6
Iischeese <129678221+Iischeese@users.noreply.github.com>
2024-05-08 14:56:20 +0000

    print

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -13,12 +13,11 @@ export default async function PrivatePage() {
   const { data: users, e } = await supabase
     .from('users')
     .select('first_name')
-    .eq('user_id', data.user.id)
     
   return (
     <>
       <section className='h-screen w-screen flex flex-col items-start justify-center px-10'>
-        <h1 className='text-5xl'>Hello, {users}</h1>
+        <h1 className='text-5xl'>Hello, {users.toString()}</h1>
       </section>
     </>
   )
