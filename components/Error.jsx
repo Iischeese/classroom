80dd597c76d717c563ddbe978e71521ab880c9cc
Iischeese <129678221+Iischeese@users.noreply.github.com>
2024-05-09 16:01:31 +0000

    fixed

--- components/Error.jsx
+++ components/Error.jsx
@@ -3,7 +3,7 @@ function Error(){
         <>
             <section className="w-screen h-screen flex flex-col items-center justify-center">
                 <h1 className="text-5xl text-red-500">An Error Occurred</h1>
-                <h2 className="text-3xl text-red-500/70">We don't know much more.</h2>
+                <h2 className="text-3xl text-red-500/70">We don&apos;t know much more.</h2>
             </section>
         </>
     )
