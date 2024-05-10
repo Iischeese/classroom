13fae595bda834c7ba23d7bf973c7d32ec70c401
Iischeese <129678221+Iischeese@users.noreply.github.com>
2024-05-10 15:26:28 +0000

    removed list

--- app/dashboard/classes/page.jsx
+++ app/dashboard/classes/page.jsx
@@ -3,14 +3,15 @@ import {user} from "@/utils/supabase/user"
 import ClassroomPreview from "@/components/dasboard/classes/ClassroomPreivew"
 import Button from "@/components/Button"
 import Input from "@/components/Input"
+
 async function Classes() {
 
-    const supabase = createClient()
+    // const supabase = createClient()
 
-    const {data: classrooms, error}  = await supabase
-        .from('classrooms')
-        .select()
-        .eq('user_id', user.data.user.id)
+    // const {data: classrooms, error}  = await supabase
+    //     .from('classrooms')
+    //     .select()
+    //     .eq('user_id', user.data.user.id)
 
     return (
         <>
@@ -21,7 +22,7 @@ async function Classes() {
                     <Button style='max-w-fit'>View</Button>
                     <Button style='max-w-fit'>Create classroom</Button>
                 </nav>
-                {classrooms.map((classroom, index) => <ClassroomPreview title={classroom.name} key={index} />)}
+                {/* {classrooms.map((classroom, index) => <ClassroomPreview title={classroom.name} key={index} />)} */}
             </section>
         </>
     )
